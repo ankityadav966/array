a81f60181672b515cce6f3c0357f8c3a148f0f5a
Ankit yadav <ankityadav941318@gmail.com>
2024-04-25 12:21:30 +0530

    ff

---
+++ Arraysecoind/arrayqustion.cpp
@@ -0,0 +1,38 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[9] = {1,2,3,4,5,6,7,8,9};
+//         // cout<< sizeof(arr)/sizeof(arr[0]) ;
+//         int end =sizeof(arr)/sizeof(arr[0])-1;
+//     for (int i = 0; i < 9; i++)
+//     {
+//         cout<<arr[i];
+//         cout<< arr[end] ;
+//         end--;
+//     }
+
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int arr[5] = {1,2,3,4,5};
+    // for (int i = 0; i < 5/2+1; i++)
+    // {
+    //     cout<<arr[i];
+    //     cout<<arr[5-1-i];
+    // }
+
+    // int length;
+    // cout << " ENter  : ";
+    // cin >> length;
+    // while (length<=5)
+    // {
+    // cout << " ReENter  : ";
+    // cin>>length;
+        
+    // }
+    int a =5/2;
+    cout<<a;
+}
