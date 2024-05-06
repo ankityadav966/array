e3d47dd5ab58cb3e12da79f565694ddf50c3dcd2
Ankit yadav <ankityadav941318@gmail.com>
2024-05-06 15:09:39 +0530

    two_array_joint

--- Arrays/unickevalue.cpp
+++ Arrays/unickevalue.cpp
@@ -1,18 +1,56 @@
+// #include <iostream>
+// using namespace std;
+// #include <vector>
+//     int findvalue(vector <int> values ){
+//         int uniquevalue = 0;
+//         for (int i = 0; i < values.size(); i++)
+//         {
+//             uniquevalue = uniquevalue ^ values[i];
+//         }
+//         return uniquevalue;
+
+//     }
+// int main(){
+//     vector<int>values = {1,1,2,3,3};
+//     int uniquevalue = findvalue(values);
+//     cout<<"Unicque value : "<< uniquevalue<<endl;
+// }
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int> arr1 = {1, 2, 3, 4, 5, 6};
+//     vector<int> arr2 = {1, 2};
+
+//     for (int i = 0; i < arr1.size(); i++)
+//     {
+//         for (int j = 0; j < arr2.size(); j++)
+//         {
+//             if (arr1[i] == arr2[j])
+//             {
+//                 cout << arr1[i]<<"   ";
+//             }
+//         }
+//     }
+// }
 #include <iostream>
-using namespace std;
 #include <vector>
-    int findvalue(vector <int> values ){
-        int uniquevalue = 0;
-        for (int i = 0; i < values.size(); i++)
+using namespace std;
+int main (){
+    vector <int> arr = {1,2,3,4,5,6};
+    vector <int> arr2 = {1,2};
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr2.size(); j++)
         {
-            uniquevalue = uniquevalue ^ values[i];
+            if (arr[i]==arr2[j])
+            {
+               cout<<arr2[j]<<"  ";
+            }
         }
-        return uniquevalue;
         
     }
-int main(){
-    vector<int>values = {1,1,2,3,3};
-    int uniquevalue = findvalue(values);
-    cout<<"Unicque value : "<< uniquevalue<<endl;   
+    
 }
-
