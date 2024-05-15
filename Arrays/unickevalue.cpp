53a14bf29f6a1c5c64a31fa52e94c789e90856c0
Ankit yadav <ankityadav941318@gmail.com>
2024-05-15 11:39:27 +0530

    shortarray_in c-++

--- Arrays/unickevalue.cpp
+++ Arrays/unickevalue.cpp
@@ -197,3 +197,42 @@
 // }
 
  
+    // #include <iostream>
+    // #include <vector>
+    // #include <algorithm>
+    // #include <bits/stdc++.h>
+    // using namespace std;
+    // int main(){
+    //     vector<int> arr = {1,2,3,4,5};
+    //     sort(arr.begin(),arr.end())
+    //     for(auto arr.auto){
+            
+    //     }
+    //     // short();
+    // }
+
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+        vector <int> arr = {4,10,8,11,1};
+        for (int i = 0; i < arr.size(); i++)
+        {
+            for (int j = 0; j < arr.size(); j++)
+            {
+                if (arr[i]<arr[j])
+                {
+                   int temp  = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = temp;
+                }
+                
+            }
+            
+        }
+        for (int i = 0; i < arr.size(); i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        
+        
+    }
